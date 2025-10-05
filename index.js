0bd6fd58bbe642047d3d15c8040e91abce7609af
Divine Lewis <divinitysoft0@gmail.com>
2025-10-05 23:44:09 +0100

    function to update the display

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ let running = false;
 const timerDisplay = document.getElementById("timer");
 const startStopBtn = document.getElementById("startStopBtn");
 const clearText = document.getElementById("clearText");
+
+function updateDisplay() {
+  const h = hours.toString().padStart(2, "0");
+  const m = minutes.toString().padStart(2, "0");
+  const s = seconds.toString().padStart(2, "0");
+  timerDisplay.textContent = `${h}:${m}:${s}`;
+}
